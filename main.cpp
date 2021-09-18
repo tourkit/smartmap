58bac7647e4f096ae1ea0eea10d15542626cdac9
Roch <roch@tourkit.net>
2021-09-19 00:35:15 +0200

    chouky

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int8_t buff[ARTNET_DMX_LENGTH +1];
 int length;
 
 int dmx_callback(artnet_node n, int port, void *d) {
-  std::cout << "OOOO" <<std::endl;
+  // std::cout << "OOOO" <<std::endl;
   uint8_t *data;
 
   data = artnet_read_dmx(n, port, &length);
@@ -38,10 +38,10 @@ int main() {
 
   artnet_set_short_name(artnet, "SmartMap");
   artnet_set_long_name(artnet, "SM");
-  // artnet_set_node_type(artnet, ARTNET_NODE); // ARTNET_SRV not working either
-  artnet_set_bcast_limit(artnet, 0);
-  artnet_set_subnet_addr(artnet, 0); //
+  artnet_set_node_type(artnet, ARTNET_NODE);
+  // artnet_set_bcast_limit(artnet, 0);
 
+  artnet_set_subnet_addr(artnet, 0); //
   artnet_set_port_type(artnet, 0, ARTNET_ENABLE_OUTPUT, ARTNET_PORT_DMX);
   artnet_set_port_addr(artnet, 0, ARTNET_OUTPUT_PORT, 3);
   // artnet_set_port_type(artnet, 0, ARTNET_ENABLE_INPUT, ARTNET_PORT_DMX);
@@ -54,8 +54,12 @@ int main() {
 
   
   // while(true) for (uint8_t i = 0; i < 10; i++) artnet_raw_send_dmx(artnet, i, data.size(), &data[0]);
-  while(true) artnet_send_dmx(artnet, 0, data.size(), &data[0]);
-  // while(true) artnet_read(artnet, 0);
+  while(true) {
+    
+    // artnet_send_dmx(artnet, 0, data.size(), &data[0]);
+    artnet_read(artnet, 1);
+
+  }
 
   artnet_stop(artnet);
   artnet_destroy(artnet);
