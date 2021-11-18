0a20f0177abf369a6c7719037a90bb0d07a3862c
Roch <roch@tourkit.net>
2021-11-18 21:03:01 +0100

    begin

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 #include "smartmap.hpp"
 
+#include "vendor/ImGuiColorTextEdit/TextEditor.h"
+
+
 #define DEBUG
 
 ArtnetDevice artnet;
@@ -37,6 +40,18 @@ bool checkFPS() { if (glfwGetTime() - lastTime <= 1./280. ) { return false; } el
 
 int main() {
 
+    	TextEditor editor;
+std::string str("test");
+			editor.SetText(str);
+            static const char* fileToEdit = "C:/Users/ebkc/Documents/testmake/web/index.html";
+	{
+		std::ifstream t(fileToEdit);
+		if (t.good())
+		{
+			std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+			editor.SetText(str);
+		}
+	}
     Atlas atlas("assets/media/");
     atlas.link(shader);
     
@@ -94,14 +109,12 @@ int main() {
     // blur->links.insert(blur_x);
     // blur->links.insert(blur_y);
 
-
     glEnable(GL_CLIP_DISTANCE0);
     glEnable(GL_CLIP_DISTANCE1);
     glEnable(GL_CLIP_DISTANCE2);
     glEnable(GL_CLIP_DISTANCE3);
 
     shader->sendUniform("MatriceUBOSize", MAT_X*MAT_Y);
-
 
     while(true) window->render([&]() {
 
@@ -145,6 +158,76 @@ int main() {
         for (auto a:fixtures[0].attributes->childrens) a->gui();
 
         ImGui::End();
+
+        		auto cpos = editor.GetCursorPosition();
+		ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
+		ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Save"))
+				{
+					auto textToSave = editor.GetText();
+					/// save text....
+				}
+				// if (ImGui::MenuItem("Quit", "Alt-F4"))
+				// 	break;
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Edit"))
+			{
+				bool ro = editor.IsReadOnly();
+				if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+					editor.SetReadOnly(ro);
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor.CanUndo()))
+					editor.Undo();
+				if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor.CanRedo()))
+					editor.Redo();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor.HasSelection()))
+					editor.Copy();
+				if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor.HasSelection()))
+					editor.Cut();
+				if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && editor.HasSelection()))
+					editor.Delete();
+				if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
+					editor.Paste();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Select all", nullptr, nullptr))
+					editor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(editor.GetTotalLines(), 0));
+
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("View"))
+			{
+				if (ImGui::MenuItem("Dark palette"))
+					editor.SetPalette(TextEditor::GetDarkPalette());
+				if (ImGui::MenuItem("Light palette"))
+					editor.SetPalette(TextEditor::GetLightPalette());
+				if (ImGui::MenuItem("Retro blue palette"))
+					editor.SetPalette(TextEditor::GetRetroBluePalette());
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, editor.GetTotalLines(),
+			editor.IsOverwrite() ? "Ovr" : "Ins",
+			editor.CanUndo() ? "*" : " ",
+			editor.GetLanguageDefinition().mName.c_str(), fileToEdit);
+
+		editor.Render("TextEditor");
+        ImGui::End();
+
+
         gui->render();
 
  
