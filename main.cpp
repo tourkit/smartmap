1d85d42904cd084facd233eb65d957be0a61822c
roch <roch@tourkit.net>
2023-07-01 15:25:08 +0200

    better range fixture

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 // #include "debug.hpp"
 // Debug debug;
 
-int main() {  
-
-    Fixture basic =  {{2},{2},{2},{2},{2},{2},{2},{2},{2},{2},{2},{2},{2,0,3.6},{2}};
+int main() { 
+    
+    Fixture basic =  {{2},{2},{2},{2},{2,-1,1},{2,-1,1},{2},{2},{2},{2},{2},{2},{2,0,360*(M_PI / 180.0)},{2}};
 
     sm.createFixtures(1,0,0,&basic);
 
