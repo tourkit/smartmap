ceb3552434eaed68c039a86eaaa28f1f098dbbf7
roch <roch@tourkit.net>
2023-07-01 11:16:25 +0200

    file refactor

--- src/file.cpp
+++ src/file.cpp
@@ -28,29 +28,26 @@ static std::vector<std::string> explode(std::string const & s, char delim) {
 
 }
 
-namespace TourKit {
+//////////////////////////////////////
+// DIRECTOPRY
+//////////////////////////////////////
 
-Directory::Directory()  {}
+Directory::~Directory()  { 
+    
+    // need to delete dir ? dont think , remove on comfirm 
+}
 
-Directory::~Directory()  { }
-
-Directory::Directory(std::string path_str) { load(path_str); }
-
-void Directory::load(std::string path_str){
-
-    this->path_str = path_str;
+Directory::Directory(std::string path) : path(path) { 
 
     struct dirent* ent;
-    
 
-    if ((dir = opendir(path_str.c_str())) != NULL) {
+    if ((dir = opendir(path.c_str())) != NULL) {
 
         while ((ent = readdir(dir)) != NULL) {
 
             std::string entryName(ent->d_name);
 
             if (entryName != ".." && entryName != ".") list.push_back(ent->d_name);
-
 
         }
 
@@ -60,57 +57,33 @@ void Directory::load(std::string path_str){
 
 }
 
-void Directory::print() {  std::cout << size() << " file(s) in \"" << path_str << "\" : \n" << '\n'; int i = 0; for (std::string file : list) std::cout << ++i << " - " << file << '\n'; }
+//////////////////////////////////////
+// FILE
+//////////////////////////////////////
 
-int Directory::size(){ return list.size(); }
 
-std::string Directory::path(int i) { return path_str+list[i]; }
-
-const std::string& Directory::operator[](int x) { return list[x]; }
-
-File::File() {  }
-
-File::File(std::string source) { load(source); }
 
 File::~File() { }
 
+File::File(std::string path) { read(path); }
 
-char* File::load(std::string source) {
-
+char* File::read(std::string path) {
 
     data.clear();
 
-    filename = source;
+    path = path;
 
-    /* V2 ... still not reloading file
-
-
-
-    FILE* f = fopen(filename, "r");
-
-    fseek(f, 0, SEEK_END);
-    size_t size = ftell(f);
-    char* ptr = new char[size+1];
-    memset(ptr,0,size+1);
-
-    fseek(f, 0, SEEK_SET);
-    fread(ptr, sizeof(char), size, f);
-
-    fclose(f);*/
-
-    /* V1*/
-
-    std::ifstream file(source.c_str(), std::ifstream::in | std::ifstream::binary);
+    std::ifstream file(path.c_str(), std::ifstream::in | std::ifstream::binary);
     file.seekg(0, std::ios::end);
     std::streampos length(file.tellg());
 
     if (length) {
 
+        auto len = (size_t)length;
         file.seekg(0, std::ios::beg);
-        size = static_cast<std::size_t>(length);
-        data.resize(size + 1);
-        memset(data.data(),0,size+1);
-        file.read(data.data(), size);
+        data.resize(len + 1);
+        memset(data.data(),0,len+1);
+        file.read(data.data(), len);
 
     }
 
@@ -120,167 +93,119 @@ char* File::load(std::string source) {
 
 }
 
-void File::write(const char* filename, std::string filestr){ write(filename,&filestr[0],filestr.length()); }
-
-void File::write(const char* filename, const char* i, int o){
+void File::write(const char* filename){
 
     std::fstream bin (filename, std::ios :: out | std::ios :: binary);
-    bin.write(i,o);
+    bin.write(&data[0],data.size());
     bin.close();
 
 }
 
-void File::print() {
 
-    std::cout << "[" << filename << "]\n" << std::endl;
-    for (int i = 0; i < size; ++i) std::cout << data[i];
-    std::cout << "\n-----------\n" << std::endl;
+//////////////////////////////////////
+// IMAGE
+//////////////////////////////////////
 
-}
+Image::Image(std::string path) { read(path); }
 
-Image::Image() {  }
+void Image::read(std::string path) {
 
-Image::Image(std::string source, bool flipped) {
-
-    if (flipped) loadflipped(source);
-    else load(source);
-    
-}
-
-Image::~Image() {  }
-
-void Image::store(int clear) {
-
-    data.clear();
-
-    data.insert(data.begin(),&i[0],&i[0]+(width*height*comp));
-
-    if (clear) stbi_image_free(i);
-
-    i = &data[0];
-
-}
-
-void Image::load(std::string source, IMAGE_ENCODING encoding, int quality) {
-
-    File file(source);
-
-    if (file.size != -1) load((unsigned char*)file.data.data(), file.size);
-
-    if (encoding != RAW) convert(encoding, quality);
-
-
-    auto filename = explode(explode(source, '/').back(), '.');
+    auto filename = explode(explode(path, '/').back(), '.');
     name = std::move(filename.front());
     extension = std::move(filename.back());
 
-}
+    // add ext check here
+    File file(path);
 
-void Image::loadflipped(std::string source) {
+    if (file.data.size() != -1) {
 
-    stbi_set_flip_vertically_on_load(true);
-    load(source);
-    stbi_set_flip_vertically_on_load(false);
+            auto pixels = stbi_load_from_memory((const stbi_uc*)&file.data[0],file.data.size(), &width, &height, &comp, 0);
+            auto count  =width*height*comp;
 
-}
+            data.clear();
+            data.resize(count);
+            memcpy(&data[0],&pixels[0],count);
 
+            stbi_image_free(pixels);
 
-void Image::load(unsigned char *i, int width, int height , int comp) {
-
-    this->i = i;
-    this->width = width;
-    this->height = height;
-    this->comp = comp;
-    this->o = width*height*comp;
-
-
-    store();
-
-}
-
-void Image::load(unsigned char *i, int o) {
-
-
-    this->i = stbi_load_from_memory(i, o, &width, &height, &comp, 0);
-
-    store(1);
-
-}
-
-void Image::convert(IMAGE_ENCODING type, int quality) {
-
-    int rv = 0;
-
-    o = 0;
-
-    data.clear();
-
-    auto callback = [](void *context, void *data, int size) {
-
-        auto _this = (Image *)context;
-        unsigned char * _data = (unsigned char *)data;
-        _this->data.insert(_this->data.end(), _data, _data + size );
-        _this->o += size;
-
-    };
-
-
-    switch (type) {
-
-        case JPEG: rv = stbi_write_jpg_to_func(callback, this, width, height, comp, i, quality); break;
-
-        case PNG: rv = stbi_write_png_to_func(callback, this, width, height, comp, i,8); break;
-
-        case BMP: rv = stbi_write_bmp_to_func(callback, this, width, height, comp, i); break;
-
-        case RAW:  break;
 
     }
 
-    i = &data[0];
+}
 
-    if (rv == 0) std::cout << "rv error\n";
+
+
+void Image::convert(Encoding type, int quality) {
+
+    // int rv = 0;
+
+    // o = 0;
+
+    // data.clear();
+
+    // auto callback = [](void *context, void *data, int size) {
+
+    //     auto _this = (Image *)context;
+    //     unsigned char * _data = (unsigned char *)data;
+    //     _this->data.insert(_this->data.end(), _data, _data + size );
+    //     _this->o += size;
+
+    // };
+
+
+    // switch (type) {
+
+    //     case JPEG: rv = stbi_write_jpg_to_func(callback, this, width, height, comp, i, quality); break;
+
+    //     case PNG: rv = stbi_write_png_to_func(callback, this, width, height, comp, i,8); break;
+
+    //     case BMP: rv = stbi_write_bmp_to_func(callback, this, width, height, comp, i); break;
+
+    //     case RAW:  break;
+
+    // }
+
+    // i = &data[0];
+
+    // if (rv == 0) std::cout << "rv error\n";
 
 }
+
 void Image::rawconvert(int destcomp) {
 
-  std::vector<unsigned char> newdata;
+//   std::vector<unsigned char> newdata;
 
-  int pixcount = width*height;
+//   int pixcount = width*height;
 
-  if (comp == 3) {
+//   if (comp == 3) {
 
-    if (destcomp > 3) {
+//     if (destcomp > 3) {
 
-      newdata.resize(pixcount*4);
+//       newdata.resize(pixcount*4);
 
-      memset(&newdata[0],255,newdata.size());
+//       memset(&newdata[0],255,newdata.size());
 
-      if (destcomp == 5) { for (int i = 0; i< pixcount; ++i)  {  newdata[i*4] = data[i*3+2]; newdata[i*4+1] = data[i*3+1];  newdata[i*4+2] = data[i*3]; } }
+//       if (destcomp == 5) { for (int i = 0; i< pixcount; ++i)  {  newdata[i*4] = data[i*3+2]; newdata[i*4+1] = data[i*3+1];  newdata[i*4+2] = data[i*3]; } }
 
-      else if (destcomp == 4) { for (int i = 0; i< pixcount; ++i)  memcpy(&newdata[i*4], &data[i*3], 3);}
+//       else if (destcomp == 4) { for (int i = 0; i< pixcount; ++i)  memcpy(&newdata[i*4], &data[i*3], 3);}
 
-    }
+//     }
 
-  }
+//   }
 
-  data = newdata;
+//   data = newdata;
 
-  comp = destcomp;
+//   comp = destcomp;
 
 }
 
 void Image::write(const char* destination, int quality) {
 
-    if (extension == "jpg") convert(JPEG,quality);
-    else if (extension == "png") convert(PNG);
-    else std::cout << "chaipaskeussétonimaj takapté ? (TourKit::Image::write)" << '\n';
+    // if (extension == "jpg") convert(JPEG,quality);
+    // else if (extension == "png") convert(PNG);
+    // else std::cout << "chaipaskeussétonimaj takapté ? (TourKit::Image::write)" << '\n';
 
-    std::cout << "exvoto" << '\n';
-    File::write(destination, (const char*)i, o);
-
-}
-
-void Image::print() { std::cout << "Image: " << name << " ." << extension << " (" << width << ", " << height << ") raw:" << (data.size()/(float)8000000) << "Mb - comp:"<< comp <<  '\n'; }
+    // std::cout << "exvoto" << '\n';
+    // File::write(destination, (const char*)&data[0], data.size());
 
 }
--- src/file.hpp
+++ src/file.hpp
@@ -30,81 +30,60 @@ static std::string readFile(const char* path) {
 
 };
 
-namespace TourKit {
 
 struct Directory {
 
-    std::string path_str;
-
+    std::string path;
     std::vector<std::string> list;
-
     DIR *dir;
 
-    Directory();
-
+    Directory(std::string path);
     ~Directory();
 
-    Directory(std::string path);
+    const std::string& operator[](int x) { return list[x]; }
+    operator std::vector<std::string>&() { return list; }
 
-    void load(std::string path);
-
-    void print();
-
-    int size();
-
-    std::string path(int i);
-
-    const std::string& operator[](int x);
 };
 
-enum IMAGE_ENCODING { JPEG, PNG, BMP, RAW, BGRA };
+struct File {
 
-class File {
-
-public:
-
-    std::string filename;
-    int size;
+    std::string path;
     std::vector<char> data;
 
-    File();
     File(std::string source);
     ~File();
 
-    char* load(std::string source);
-    static void write(const char* filename, std::string filestr);
-    static void write(const char* filename, const char* i, int o);
-    void print();
+    char* read(std::string source);
+    void write(const char* path);
+
+    const char& operator[](int x) { return data[x]; }
+    operator std::vector<char>&() { return data; }
+
 };
 
-class Image {
 
-public:
 
-    unsigned char *i;
-    int width = 0, height, comp, o;  // comp: 1 = lumi, 2 = ?, 3 = rgb, 4 = rgba, 5 = bgra
-    std::vector<unsigned char> data;
+struct Image {
+
+    enum Encoding { JPEG, PNG, BMP, RAW, BGRA };
 
     std::string name,extension;
 
-    Image();
-    Image(std::string source, bool flipped = false);
-    ~Image();
+    int width = 0, height, comp;  // comp: 1 = lumi, 2 = ?, 3 = rgb, 4 = rgba, 5 = bgra
 
-    void store(int clear = 0);
-    void load(std::string source, IMAGE_ENCODING encoding = RAW, int quality = 80);
-    void loadflipped(std::string source);
-    void load(unsigned char *i, int width, int height , int comp);
-    void load(unsigned char *i, int o);
-    void convert(IMAGE_ENCODING type, int quality = 80);
+    std::vector<unsigned char> data;
+
+    Image(std::string source);
+    // ~Image();
+
+    void read(std::string source);
+
+    void convert(Encoding type, int quality = 80);
 
     void rawconvert(int destcomp);
+
     void write(const char* destination, int quality = 100);
-    void print();
+
 };
-
-}
-
-using Image = TourKit::Image;
 
 #endif
