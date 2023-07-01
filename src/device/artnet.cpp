521838d5affbd01beb53af7432a7022945293dc6
roch <roch@tourkit.net>
2023-07-01 15:09:19 +0200

    artnet split

--- src/device/artnet.cpp
+++ src/device/artnet.cpp
@@ -1,1 +1,18 @@
 #include "artnet.hpp"  
+
+Artnet::Artnet(const char* ip) {
+
+artnet = artnet_new(ip, 0);
+artnet_set_short_name(artnet, "SmartMap");
+artnet_set_long_name(artnet, "SmartMap");
+artnet_start(artnet);
+artnet_set_dmx_handler(artnet, [](artnet_node n, artnet_packet p, void *_this){
+    for(int i = 0; i < __builtin_bswap16((uint16_t&)p->data.admx.lengthHi); ++i) ((Artnet*)_this)->universes[p->data.admx.universe].raw[i] = p->data.admx.data[i]; 
+    return 1; 
+}, this);
+
+}
+
+Artnet::~Artnet() { artnet_stop(artnet); artnet_destroy(artnet); }
+
+void Artnet::run() { artnet_read(artnet, .1); for (auto u:universes)  u.second.callback(&u.second); }
--- src/device/artnet.hpp
+++ src/device/artnet.hpp
@@ -56,25 +56,13 @@ struct Artnet {
   };
 
   artnet_node artnet;
-
   std::map<uint16_t, Universe> universes;
 
-  Artnet(const char* ip) {
-    
-    artnet = artnet_new(ip, 0);
-    artnet_set_short_name(artnet, "SmartMap");
-    artnet_set_long_name(artnet, "SmartMap");
-    artnet_start(artnet);
-    artnet_set_dmx_handler(artnet, [](artnet_node n, artnet_packet p, void *_this){
-      for(int i = 0; i < __builtin_bswap16((uint16_t&)p->data.admx.lengthHi); ++i) ((Artnet*)_this)->universes[p->data.admx.universe].raw[i] = p->data.admx.data[i]; 
-      return 1; 
-    }, this);
-  
-  }
+  Artnet(const char* ip);
 
-  ~Artnet() { artnet_stop(artnet); artnet_destroy(artnet); }
+  ~Artnet();
   
-  void run() { artnet_read(artnet, .1); for (auto u:universes)  u.second.callback(&u.second); }
+  void run();
 
 } ;
 
