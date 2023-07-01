f91b726d4d175c598278a79dc0abd1d28bbdd474
roch <roch@tourkit.net>
2023-07-01 18:33:01 +0200

    quickfix

--- src/smartmap.cpp
+++ src/smartmap.cpp
@@ -35,7 +35,7 @@ SmartMap::SmartMap() {
 
     
     shader->use();
-    atlas = new Atlas("");
+    atlas = new Atlas("assets/media/");
     atlas->link(shader);
 
 }
@@ -108,12 +108,13 @@ void SmartMap::render() {
         sm.passBuf->bind();
         sm.shader->use();
 
+        // feedback
         glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_SRC_COLOR);
         sm.quadA->draw(sm.MATS); // quantity is instances count in shader 
         glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
         sm.quadB->draw(sm.MATS); // quantity is instances count in shader 
 
-        // sm.passBuf->copy(*sm.outBuf);
+        // sm.passBuf->copy(sm.outBuf);
 
         // glBindImageTexture(0, *sm.outBlur, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8);
         // glBindImageTexture(1, *sm.outBuf, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA8);
@@ -123,7 +124,7 @@ void SmartMap::render() {
 
         sm.winFB->clear(); 
         
-        sm.outBuf->bind();
+        sm.passBuf->bind();
         sm.shader->use();
         sm.quadA->draw();
 
