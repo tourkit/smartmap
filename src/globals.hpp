76b510677a2b65b1777714daa09803f0228c696b
roch <roch@tourkit.net>
2023-07-15 18:44:21 +0200

    addChar

--- src/globals.hpp
+++ src/globals.hpp
@@ -30,12 +30,47 @@
 
 #include "file.hpp"
 
-	template<typename T>
-	using Ref = std::shared_ptr<T>;
-	template<typename T, typename ... Args>
-	constexpr Ref<T> newRef(Args&& ... args) {
-		return std::make_shared<T>(std::forward<Args>(args)...);
-	}
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+struct FT { 
+
+    FT_Library library;
+    FT_Face face;
+    FT_GlyphSlot slot;
+
+    GLuint width;
+    GLuint height;
+    
+    void* buffer;
+
+    FT(const char* chr, int size = 200) {
+        
+        FT_Init_FreeType(&library);
+        
+        FT_New_Face(library, "C:/msys64/home/SysErr/old/smartmap/assets/Anonymous.ttf", 0, &face); 
+
+        FT_Set_Pixel_Sizes(face, 0, size);
+        
+        slot = face->glyph;
+        FT_Load_Char(face, *chr, FT_LOAD_RENDER);
+
+        buffer = slot->bitmap.buffer;
+        width = slot->bitmap.width;
+        height = slot->bitmap.rows;
+
+    }
+
+    ~FT() {
+        
+        FT_Done_Face(face);
+        FT_Done_FreeType(library);
+
+    }
+
+};
+
+
 
 struct vec2 { float x = 0, y = 0; };
 struct vec4 { float x = 0, y = 0, z = 0, w = 0; };
