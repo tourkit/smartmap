b55c57565822354fa7e24ed77e9f07e1af4f6f78
roch <roch@tourkit.net>
2023-08-03 18:46:11 +0200

    base

--- main.cpp
+++ main.cpp
@@ -4,46 +4,45 @@
 
                         */
 
-#include "smartmap.hpp"
-// #include "boi/lerplate.hpp"
+// #include "smartmap.hpp"
+#include "boilerplate.hpp"
 // #include "debug.hpp"
 // Debug debug;
 
-SmartMap& sm = SmartMap::getInstance();
+// SmartMap& sm = SmartMap::getInstance();
 
 int main() { 
 
-    // Boilerplate();
+    Boilerplate();
     
-    SmartMap::Fixture basic = {
-        {1},{1},{1},{1},    // DIM RGB
-        {2},{2},  // POSITION
-        {2},{2},            // FOCUS
-        {1},{1},{1},{1},    // GOBO
-        {1,0,6.28318530718},{1},{1}, // BEAM : Orientation, Strobe, Feedback
-        {0} // pr buffer alignment vec4 dans glsl
-    }; 
+    // SmartMap::Fixture basic = {
+    //     {1},{1},{1},{1},    // DIM RGB
+    //     {2},{2},  // POSITION
+    //     {2},{2},            // FOCUS
+    //     {1},{1},{1},{1},    // GOBO
+    //     {1,0,6.28318530718},{1},{1}, // BEAM : Orientation, Strobe, Feedback
+    //     {0} // pr buffer alignment vec4 dans glsl
+    // }; 
 
-    SmartMap::Layer l1(0, 0, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 5, 5);
-    // SmartMap::Layer l2(0, 1, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 2,2);
+    // SmartMap::Layer l1(0, 0, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 5, 5);
+    // // SmartMap::Layer l2(0, 1, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 2,2);
     
-    // SmartMap::Layer l2(0, 0, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
+    // // SmartMap::Layer l2(0, 0, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
 
-    for (int i = 0; i < 5; i++) { 
+    // for (int i = 0; i < 5; i++) { 
 
-        auto j = i*4;
+    //     auto j = i*4;
 
-        for (int k = 0; k < 4; k++) { std::cout << sm.matriceUBO->data[j+k] << " ";  }
+    //     for (int k = 0; k < 4; k++) { std::cout << sm.matriceUBO->data[j+k] << " ";  }
 
-        std::cout << "" << std::endl;
+    //     std::cout << "" << std::endl;
 
 
-     }
+    //  }
 
-    sm.render();
+    // sm.render();
  
 }
-
 
 
 
