6464983f01893d033f691c2100d660bf560d531b
roch <roch@tourkit.net>
2023-08-09 23:01:54 +0200

    main proper

--- main.cpp
+++ main.cpp
@@ -21,9 +21,9 @@ int main() {
 
     SmartMap::Layer l1(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 5, 5);
 
-    // SmartMap::Layer l2(0, 1, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 2,2);
+    // SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 2,2);
     
-    // SmartMap::Layer l2(0, 0, basic, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
+    // SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
 
     sm.render();
  
