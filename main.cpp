77c89a83131a20eb74e54f9db8a77ecb9db68431
roch <roch@tourkit.net>
2023-08-11 12:25:56 +0200

    multi layer attemp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 // Debug debug;
 
 
-
+1
 int main() { 
 
     // Boilerplate();
@@ -19,9 +19,9 @@ int main() {
 
     SmartMap& sm = SmartMap::getInstance();
 
-    // SmartMap::Layer l1(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 5, 5);
+    SmartMap::Layer l1(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 2, 2);
 
-    SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,4);
+    SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,1);
     
     // SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
 
