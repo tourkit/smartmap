ae407541f3da4037f721626b5fda0ec1efbe4bff
roch <roch@tourkit.net>
2023-08-18 09:19:36 +0200

    json wip

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
                         */
 
-#include "smartmap.hpp"
+// #include "smartmap.hpp"
+#include "engine.hpp"
 #include "boilerplate.hpp"
 // #include "debug.hpp"
 // Debug debug;
@@ -15,17 +16,25 @@ int main() {
 
     // Boilerplate();
 
-    Component::init();
 
-    SmartMap& sm = SmartMap::getInstance();
+    // SmartMap& sm = SmartMap::getInstance();
 
-    SmartMap::Layer l1(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 2, 2);
+    // SmartMap::Layer l1(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Grid, 4,1);
 
-    SmartMap::Layer l2(80, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,1);
+    // SmartMap::Layer l2(80, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,1);
     
     // SmartMap::Layer l2(0, 0, sm.basic_fixture, sm.window->width,sm.window->height, SmartMap::Layer::Mode::Free, 4,2);
 
-    sm.render();
+
+
+    Engine::Run();
+
+
+    // while(true) window->render([&]() {
+
+    // }
+
+    // sm.render();
  
 
 
