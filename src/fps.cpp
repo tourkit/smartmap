22f5eb011b2fe22e24235fe1f5684e777fb9080f
roch <roch@tourkit.net>
2023-12-14 09:35:24 +0100

    fps struct

---
+++ src/fps.cpp
@@ -0,0 +1,28 @@
+#include "fps.hpp"
+
+#include <chrono>
+#include <thread>
+
+
+FPS::FPS(std::string name, float max_fps) : name(name), max_fps(max_fps) { 
+    
+    pool.push_back(this); 
+    
+}
+
+float FPS::get() {
+    current_time = glfwGetTime();    
+    fps = 1./(current_time-last_time);
+    return fps;
+}
+
+float FPS::run(float max) {
+
+    if (!max) max = max_fps;
+    if (max) while ( get() > max+1) std::this_thread::sleep_for(std::chrono::microseconds(10));
+    last_time = current_time;
+    if (has_dropped) has_dropped = false;
+    if (fps<max) has_dropped = true;
+    return fps;
+
+}
---
+++ src/fps.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "pch.hpp"
+
+
+struct FPS {
+
+    std::string name;
+
+    static inline std::vector<FPS*> pool;
+
+    FPS(std::string name = "FPS", float max_fps = 0);
+
+    float current_time = 0, last_time = 0, fps = 1, max_fps = 0;
+    
+    bool has_dropped = false;
+
+    float get();
+    float run(float max = 0);
+
+    operator float() { return fps; }
+
+};
