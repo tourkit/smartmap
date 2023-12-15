da55223d6608173ce277078b274a66e4b1bc469a
roch <roch@tourkit.net>
2023-12-15 17:48:25 +0100

    new staack

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,8 +1,57 @@
 #include "engine.hpp"
 #include "stack.hpp"
+///////////////////// STACK
 
+
+
+
+///////////////////// CUE
+
+Stack::Cue::~Cue() { if (owner) owner->remove(this); }
+
+Stack::Cue* Stack::Cue::prepend(Cue* cue) {
+
+    return owner->add(cue, std::distance(owner->list.begin(), std::find(owner->list.begin(), owner->list.end(), this)));
+}
+
+Stack::Cue* Stack::Cue::append(Cue* cue) {
+
+    return owner->add(cue, std::distance(owner->list.begin(), std::find(owner->list.begin(), owner->list.end(), this))+1);
+
+}
+
+void  Stack::Cue::up() { 
+    
+    if (!owner) return;
+
+
+    auto it = std::find(owner->list.begin(), owner->list.end(), this);
+    int index = std::distance(owner->list.begin(), it);
+
+    if(index < 1) return; 
+
+    owner->list.erase(it);
+    owner->list.insert(owner->list.begin() + index - 1, this);
+
+}
+
+void  Stack::Cue::down() {
+
+    if (!owner) return;
+    
+    auto it = std::find(owner->list.begin(), owner->list.end(), this);
+    int index = std::distance(owner->list.begin(), it);  
+
+    if(index > owner->list.size()-2) return;
+
+    owner->list.erase(it);
+    owner->list.insert(owner->list.begin() + index + 1, this); 
+
+}
 
 void Stack::Cue::run() {  } 
+
+///////////////////// STACKCUE
 
 Stack::StackCue::StackCue(Stack *stack, std::string name) : stack(stack) { 
   
@@ -14,12 +63,14 @@ void Stack::StackCue::run() {
     stack->run();
 
 }
+///////////////////// CLEARCUE
 
 Stack::ClearCall::ClearCall(FrameBuffer *fb, std::string name)  { 
   
     this->fb = fb;
 
 }
+///////////////////// DRAWCUE
 
 Stack::DrawCall::DrawCall(VBO* vbo, ShaderProgram *shader, Texture *texture, FrameBuffer *fb, std::string name) 
  
@@ -49,6 +100,7 @@ void Stack::ClearCall::run() {
 
 }
 
+///////////////////// ActionCue
 
 Stack::Action::Action(std::function<void()> callback, std::string name) 
 
--- src/stack.hpp
+++ src/stack.hpp
@@ -23,12 +23,23 @@ struct Stack {
 
     Stack* parent = nullptr;
     std::vector<Stack*> childrens;
+    std::string name = "Stack"; 
     
 
     struct Cue { 
         
         std::string name = "Cue"; 
+        Stack* owner = nullptr;
         bool active = true;
+
+        Cue* prepend(Cue* cue);
+        Cue* append(Cue* cue);
+
+        ~Cue();
+
+        void up();
+        void down();
+
         virtual void run() ; 
         
     };
@@ -77,6 +88,66 @@ struct Stack {
         void run() override;
 
     };
+
+    Cue* add(Cue* cue, int index = -1) { 
+
+        cue->owner = this; 
+        
+        auto it = list.end();
+        if (index >-1) it = list.begin()+index;
+        list.insert(it, cue);
+
+        return cue;
+
+    }
+
+    void remove(Cue* cue) { 
+
+        cue->owner = nullptr;  
+
+        list.erase(std::find(list.begin(), list.end(), cue)); 
+        
+    }
+
+    void up() { 
+        
+        if (!parent) return;
+
+        auto it = std::find(parent->childrens.begin(), parent->childrens.end(), this);
+        int index = std::distance(parent->childrens.begin(), it);
+
+        if(index<1) return; 
+
+        parent->childrens.erase(it);
+        parent->childrens.insert(parent->childrens.begin() + index - 1, this);
+    
+    }
+
+    void down() {
+
+        if (!parent) return;
+        
+        auto it = std::find(parent->childrens.begin(), parent->childrens.end(), this);
+        int index = std::distance(parent->childrens.begin(), it);  
+
+        if(index > parent->childrens.size()-2) return;
+
+        parent->childrens.erase(it);
+        parent->childrens.insert(parent->childrens.begin() + index + 1, this); 
+
+    }
+
+    Stack* child(std::string name) { 
+
+        for (auto c : childrens) if (c->name == name) return c;
+        
+        childrens.push_back(new Stack{});
+        childrens.back()->name = name;        
+        childrens.back()->parent = this;
+
+        return childrens.back();
+
+    }
 
     void run();
 
