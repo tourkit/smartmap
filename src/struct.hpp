7679b1ad512ac82acb324435b1289fc36386b4ca
roch <roch@tourkit.net>
2024-03-06 16:47:50 +0100

    include

--- src/struct.hpp
+++ src/struct.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <unordered_set>
 
-struct Component;
+#include "component.hpp"
 
 static int nextFactor(int x, int factor = 4) { return ((int)((x-1)/(float)factor)+1)*factor*!!x; }
 struct Struct { 
