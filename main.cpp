55dff114ed225cb1a139dacd23774f0e067528d1
roch <roch@tourkit.net>
2024-03-06 17:19:35 +0100

    very clean

--- main.cpp
+++ main.cpp
@@ -5,15 +5,12 @@
                         */
 
 #include "engine.hpp"
-#include "test.hpp"
 
 int main() {
 
     engine.init();
 
     engine.open("project2.json");
-
-    Test test;
 
     engine.run();
 
