467276c15f54bc35bbbfd25a4c79aad8da14083f
roch <roch@tourkit.net>
2024-03-12 09:17:18 +0100

    copy seems cool

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@
 #include <set>
 #include <typeindex>
 
+
 #include "struct2.hpp"
 #include "buffer2.hpp"
 #include "instance2.hpp"
+
 
 struct ui { uint32_t v; };
 struct float_ { float v; };
@@ -21,13 +23,46 @@ struct vec2 { float x = 0, y = 0; };
 struct vec3 { float x = 0, y = 0, z = 0; };
 struct vec4 { float x = 0, y = 0, z = 0, w = 0; };
 
+namespace TEST {
+
+std::string glsl_layout(Member& s) {
+
+    auto bkp_srtiding = s.striding();
+
+    s.striding(true);
+
+    if (!s.members.size()) return "";
+
+    std::string str = "struct " + s.name() + " {";
+
+    for (auto &m : s.members) {
+        
+        std::string name = !m->typed() ? "struct" : m->type().name();
+    
+        str += " " + name + " " + m->name() + ";";
+    
+    }
+
+    for (int i = 0; i < s.stride()/sizeof(float); i++) str += " float stride" + std::to_string(i) + ";";
+    
+    str += " };";
+    
+    s.striding(bkp_srtiding);
+
+    return str;
+
+}
+
+};
+
 int main() {
 
 using namespace TEST;
 
     logger.cout();
 
-    Struct& Rect = Struct::create("Rect").add<vec2>("pos").add<vec2>("size");
+    // Struct& rectangle = Struct::create("rectangle").add<vec2>("reca").add<vec2>("recb");
+    Struct& Rect = Struct::create("Rect").add<vec2>("pos").add<vec2>("size");//.add(rectangle);
     
     Buffer buff;
 
@@ -37,16 +72,35 @@ using namespace TEST;
 
     buff.add(quad);
 
+    // buff.print();
+    
+    buff["myquad"].eq(1)["Rect"]["size"].set<uint32_t>(123);
+
+
     PLOGD << "BKP";
 
     auto bkp = buff.copy();
 
-    /// is myquad copyed ? compre adresses
+    PLOGD <<buff["myquad"].member;
 
-    PLOGD << (buff["myquad"].member == bkp["myquad"].member ? "ARGH" : "OUF");
+    PLOGD <<bkp["myquad"].member;
+
+    // bkp["myquad"].eq(1)["Rect"]["size"].set<uint32_t>(245);
+
+    // PLOGD<<buff["myquad"].eq(1)["Rect"]["size"].get<uint32_t>();
+
+    // PLOGD<<bkp["myquad"].eq(1)["Rect"]["size"].get<uint32_t>();
+
+    // buff.remap(bkp);
+
+    hard_delete(&bkp);
+
+    // PLOGD<<buff["myquad"].eq(1)["Rect"]["size"].get<uint32_t>();
+
 
     PLOGD <<"out";
 
-    return 0;
  
 }
+
+
