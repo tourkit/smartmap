6901485523839d3055836b81bd59f9b9de61e2a7
rjub <rjub@pm.me>
2024-07-14 07:23:17 -0400

    open.hpp

---
+++ src/open.hpp
@@ -0,0 +1,22 @@
+
+#include <cstring>
+
+#include "json.hpp"
+
+struct Open{
+
+    void json(std::string path);
+
+    JSON json_v;
+
+
+    void medias();
+    void inputs();
+    void layers();
+    void effectors();
+    void models();
+    void outputs();
+    void uberlayers();
+    void editors();
+    
+};
