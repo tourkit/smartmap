02ca363b67d177226dcb5b5cce2924ee1d892932
rjub <rjub@pm.me>
2024-07-25 05:16:47 -0400

    naming fix

--- src/open.cpp
+++ src/open.cpp
@@ -186,7 +186,7 @@ static void addEffectors(JSONVal v, Node* node) {
         if (! effector_)   { PLOGW << "not an FX : " << effector_def.str(); continue; }
         
         auto new_ = node->add(effector_);
-        if (new_) new_->name(effector_def.str()); 
+        if (new_) new_->name(effector_def.name()); 
      
      }
 
