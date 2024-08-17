f4b0aa7e002e4d43f29ad9b1ae0cf4cc18042996
rjub <rjub@pm.me>
2024-08-17 12:18:17 -0400

    bilding

--- main.cpp
+++ main.cpp
@@ -6,6 +6,36 @@
 
 #include "src/engine.hpp"
 
+#include "src/effector.hpp"
+
+// maybe I need Member::Link to tell StrobeFX::static_s when StrobeFX::s has changed
+
+// check how Member::update() works 
+
+// could I use referings ?
+
+struct StrobeEffector : Effector {
+
+    int step = 2; 
+
+    Member* strobe_s;
+    
+    ~StrobeEffector() { delete strobe_s; }
+
+    StrobeEffector() {
+
+        strobe_s = engine.glsl_data.add<int>("strobe").members.back();
+
+    }
+
+    bool setup(Builder* builder) override;
+    
+    bool body(Builder* builder, std::string prepend = "") override;
+
+
+};
+
+
 int main() {
 
     engine.init();
@@ -13,6 +43,8 @@ int main() {
     logger.cout(Sev::warning);
 
     engine.open("./project.json");
+
+
 
     engine.run();
 
