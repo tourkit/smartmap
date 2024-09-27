9055ed1215ab5e848d67bcdb39fb7e9aca79cc31
rjub <rjub@pm.me>
2024-09-27 09:07:58 -0400

    nioops

--- src/widgets.hpp
+++ src/widgets.hpp
@@ -18,7 +18,7 @@ void TextX(std::string label, int offset, int size, int depth, std::vector<float
 bool DimWiget(uint32_t* x, uint32_t* y, std::string append = "") ;
 void DefWidget(Member *member_, int offset = 0, int depth = 0);
 bool RawWidget(void *data, size_t size);
-bool SlidersWidget(Member* buff, Member* member, uint32_t offset);
+bool SlidersWidget(Member* buff, Member* member=nullptr, uint32_t offset=0);
 bool IntButtons(int* p_data );
 bool VCharSlider(void* c, int label_id = 0) ;
 
